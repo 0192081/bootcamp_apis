f2bb7b90a2e02babe2f5f10b9615144f8cbdefc4
0192081 <thoko.sibeko2@gmail.com>
2022-09-01 14:58:17 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+
+import  express from 'express';
+
+import longestWord from './bootcamp/longestWord.js'
+import shortestWord from './bootcamp/shortestWord.js'
+import wordLengths from './bootcamp/wordLengths.js'
+import totalPhoneBill from './bootcamp/totalPhoneBill.js'
+import transportFee from './bootcamp/transportFee.js'
+import enoughAirtime from './bootcamp/enoughAirtime.js'
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/api/word_game', function (req, res){
+    const sentence = req.query.sentence;
+console.log(sentence)
+    if(!sentence){
+        res.json({
+        error : 'Please send in a sentence to analyse'
+        })
+    }
+    
+    res.json({
+        "longestWord" : longestWord(sentence),
+        "shortestWord" : shortestWord(sentence),
+        "sum" : wordLengths (sentence)
+    });
+});
+
+const PORT = 6007;
+app.listen (PORT,function(){
+    console.log('api started on port', PORT)
+})
+
+
+
+
+
